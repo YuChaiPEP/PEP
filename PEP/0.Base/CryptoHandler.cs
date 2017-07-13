777571d3b2de85e4f938a8ea8df2921ebb6bdd2f
Young <yy19920519@sina.com>
2017-07-13 11:05:46 +0800

    function: DES crypto

--- PEP/0.Base/CryptoHandler.cs
+++ PEP/0.Base/CryptoHandler.cs
@@ -18,5 +18,54 @@ namespace PEP
             var encs = md5.ComputeHash(data);
             return BitConverter.ToString(encs).Replace("-", "");
         }
+        public static string DESEncrypt(string input, string key, ref bool success)
+        {
+            byte[] btKey = Encoding.UTF8.GetBytes(key);
+            byte[] btIV = Encoding.UTF8.GetBytes(key);
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                byte[] inData = Encoding.UTF8.GetBytes(input);
+                try
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(btKey, btIV), CryptoStreamMode.Write))
+                    {
+                        cs.Write(inData, 0, inData.Length);
+                        cs.FlushFinalBlock();
+                    }
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+                catch
+                {
+                    success = false;
+                    return input;
+                }
+            }
+        }
+
+        public static string DESDecrypt(String input, string key, ref bool success)
+        {
+            byte[] btKey = Encoding.UTF8.GetBytes(key);
+            byte[] btIV = Encoding.UTF8.GetBytes(key);
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                byte[] inData = Convert.FromBase64String(input);
+                try
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(btKey, btIV), CryptoStreamMode.Write))
+                    {
+                        cs.Write(inData, 0, inData.Length);
+                        cs.FlushFinalBlock();
+                    }
+                    return Encoding.UTF8.GetString(ms.ToArray());
+                }
+                catch
+                {
+                    success = false;
+                    return input;
+                }
+            }
+        }
     }
 }
