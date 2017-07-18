fc070b06b43593594150d76768b27ea62bb93472
现 吴 <thuwx95@gmail.com>
2017-07-18 10:39:52 +0800

    add FileManager

---
+++ PEP/1.Back/FileManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEP
+{
+    class FileManager
+    {
+        private FtpHandler ftp;
+        private string IP = "192.168.158.117";
+        private string username = "";
+        private string password = "";
+
+        public FileManager()
+        {
+            ftp = new FtpHandler(IP, username, password);
+        }
+
+        public void uploadLogFile(string filename)
+        {
+
+        }
+
+        public void downloadLogFile(string filename)
+        {
+
+        }
+
+        public void uploadTaskFile(string filename)
+        {
+
+        }
+
+        public void downloadTaskFile(string filename)
+        {
+
+        }
+    }
+}
